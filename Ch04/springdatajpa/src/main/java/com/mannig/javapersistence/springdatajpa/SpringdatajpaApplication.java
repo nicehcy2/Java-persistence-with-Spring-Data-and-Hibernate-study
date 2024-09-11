4d63c51e3f9bc4b9da3482dba26f49b512bd41c5
nicehcy <nicehcy2@naver.com>
2024-09-11 21:30:41 +0900

    feat: ch04-1

--- Ch04/springdatajpa/src/main/java/com/mannig/javapersistence/springdatajpa/SpringdatajpaApplication.java
+++ Ch04/springdatajpa/src/main/java/com/mannig/javapersistence/springdatajpa/SpringdatajpaApplication.java
@@ -1,7 +1,14 @@
 package com.mannig.javapersistence.springdatajpa;
 
+import com.mannig.javapersistence.springdatajpa.model.User;
+import com.mannig.javapersistence.springdatajpa.repositories.UserRepository;
+import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import java.time.LocalDate;
+import java.time.Month;
 
 @SpringBootApplication
 public class SpringdatajpaApplication {
@@ -10,4 +17,20 @@ public class SpringdatajpaApplication {
 		SpringApplication.run(SpringdatajpaApplication.class, args);
 	}
 
+	/*
+	@Bean
+	public ApplicationRunner configure(UserRepository userRepository) {
+
+		return env ->
+		{
+			User user1 = new User("beth", LocalDate.of(2020, Month.AUGUST, 3));
+			User user2 = new User("mike", LocalDate.of(2020, Month.JANUARY, 18));
+
+			userRepository.save(user1);
+			userRepository.save(user2);
+
+			userRepository.findAll().forEach(System.out::println);
+		};
+	}
+	 */
 }
